ec5b51441480b44f7d7ae9fe0796911a70955369
balo-max <shoni6124@gmail.com>
2024-11-20 20:45:58 +0100

    add task-3-fix

--- js/task-2.js
+++ js/task-2.js
@@ -1,13 +1,14 @@
 const calcAverageCalories = function (days) {
     let totalCalories = 0;
-
+    
     if(days.length === 0) {
         return 0;
     }
-    
+
     for (const day of days) {
         totalCalories += day.calories;
     }
+    
     const averageCalories = totalCalories / days.length;
     return averageCalories;
 }
